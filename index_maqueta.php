70d1fb94a6e7c79b865a8443626ee116e8a53d05
Juan de la cruz <jcruzgarrido@gmail.com>
2024-11-07 19:48:00 +0100

    maquetacion inicial y MVC
    Maquetación inicial CSS y inicio de desglose MVC

---
+++ index_maqueta.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tienda de camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <!-- Cabecera -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta logo">
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+        </header>
+
+        <!-- Menu -->
+        <nav id="menu">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Categoria 1</a></li>
+                <li><a href="#">Categoria 2</a></li>
+                <li><a href="#">Categoria 3</a></li>
+                <li><a href="#">Categoria 4</a></li>
+                <li><a href="#">Categoria 5</a></li>
+            </ul>
+        </nav>
+
+        <div id="content">
+            <!-- Barra lateral -->
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
+                    <form action="#" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
+                        <input type="submit" value="Enviar">
+                    </form>
+                    <ul>
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                        <li><a href="#">Gestionar categorias</a></li>
+                    </ul>
+                </div>
+            </aside>
+
+            <!-- Contenido central -->
+            <div id="central">
+                <h1>Productos destacados</h1>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta azul ancha</h2>
+                    <p>30 euros</p>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta azul ancha</h2>
+                    <p>30 euros</p>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta azul ancha</h2>
+                    <p>30 euros</p>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+            </div>
+            <!--pie de pagina-->
+            <footer id="footer">
+                <p>Desarrollado por mi &copy; <?= date('Y') ?></p>
+            </footer>
+        </div>
+
+</body>
+
+</html>
